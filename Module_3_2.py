a497c6940060063f8732c95e447089849d3f7234
WonderStasy <stanislavflowers@ya.ru>
2024-10-15 20:35:58 +0300

    Update Module_3_2.py

--- Module_3_2.py
+++ Module_3_2.py
@@ -1,9 +1,9 @@
 def send_email(message, recipient, *, sender = 'univercity.help@gmail.com'):
     if ('@' and '.com' or '.ru' or '.net') not in recipient and ('@' and '.com' or '.ru' or '.net') not in sender:
         print("Невозможно отправить письмо с адреса <sender> на адрес <recipient>.")
-    if sender == recipient:
+    elif sender == recipient:
         print('Нельзя отправить письмо самому себе!')
-    if sender == 'university.help@gmail.com':
+    elif sender == 'university.help@gmail.com':
         print('Письмо успешно отправлено с адреса <sender> на адрес <recipient>.')
     else:
         print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.')
